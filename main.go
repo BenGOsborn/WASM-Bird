bf5c36ff3176f82a1db26aebdab99a9d4ebdacbd
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-12 21:47:12 +1000

    Update

--- main.go
+++ main.go
@@ -4,23 +4,6 @@ import (
 	"fmt"
 )
 
-func inc(x *int) {
-    *x++;
-}
-
 func main() {
-    y := 3;
-
-    inc(&y);
-
-    fmt.Println(y);
-
-    switch (3) {
-    case 3:
-        fmt.Println(3);
-    case 4:
-        fmt.Println(4);
-    default:
-        fmt.Println(5);
-    }
+	fmt.Println("Hello world");
 }

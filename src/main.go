03142ef8ea7a54d40b684f3db76b2a319f240738
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-12 20:20:04 +1000

    Update

--- src/main.go
+++ src/main.go
@@ -14,4 +14,13 @@ func main() {
     inc(&y);
 
     fmt.Println(y);
+
+    switch (3) {
+    case 3:
+        fmt.Println(3);
+    case 4:
+        fmt.Println(4);
+    default:
+        fmt.Println(5);
+    }
 }

29e3faeb304630344673c66cf0c009f2561a8c3f
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-12 22:16:16 +1000

    Update

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.use("/", express.static(__dirname + "/public"));
+// Serve static content
+app.use("/", express.static(__dirname + "/static"));
 
 const PORT = process.env.PORT || 3000;
 

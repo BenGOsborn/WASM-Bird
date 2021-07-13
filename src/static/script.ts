9b57392d08cfb13c7c31dc65baf82f910b385000
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-13 10:59:38 +1000

    Update

--- src/static/script.ts
+++ src/static/script.ts
@@ -1,1 +1,12 @@
-import "./wasm_exec";
+(async () => {
+    // Initialize the Go code
+    const go = new Go();
+
+    const result = await WebAssembly.instantiateStreaming(
+        fetch("main.wasm"),
+        go.importObject
+    );
+    go.run(result.instance);
+
+    console.log(add(1, 2, 4)); // It is not loaded - I need to wait for it to be loaded in
+})();

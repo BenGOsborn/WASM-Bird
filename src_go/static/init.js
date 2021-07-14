7e9c68f82cd7c5b98fa7fcf0ca67c31eea818804
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-14 16:04:08 +1000

    Update

--- src_go/static/init.js
+++ src_go/static/init.js
@@ -1,9 +1,7 @@
-(async () => {
-    // Initialize the Go WASM code
-    const go = new Go();
-    const result = await WebAssembly.instantiateStreaming(
-        fetch("main.wasm"),
-        go.importObject
-    );
-    go.run(result.instance);
-})();
+// Initialize the Go WASM code
+const go = new Go();
+const result = await WebAssembly.instantiateStreaming(
+    fetch("main.wasm"),
+    go.importObject
+);
+go.run(result.instance);

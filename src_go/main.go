4715283e4d1ab81e3825ffbad89005d0a445a40a
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-14 20:29:43 +1000

    Update

--- src_go/main.go
+++ src_go/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"syscall/js"
 )
 
+func add(this js.Value, args []js.Value) interface{} {
+	total := 0
+
+	for _, value := range args {
+		total += value.Int()
+	}
+
+	return js.ValueOf(total)
+}
+
+func addEventListener(elementID string, event string) {
+	callback := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		fmt.Println(args)
+
+		return nil
+	})
+	js.Global().Get("document").Call("getElementById", elementID).Call("addEventListener", "click", callback)
+}
+
 func main() {
-	fmt.Println("Hello world from GO!");
+	// Channel used to prevent program terminating
+	c := make(chan struct{}, 0)
+
+	fmt.Println("Hello world from GO!")
+
+	// Add an event listener
+	addEventListener("canvas", "click")
+
+	// Expose functions
+	js.Global().Set("add", js.FuncOf(add))
+
+	// Prevent the program from terminating
+	<-c
 }
--- src_go/static/init.js
+++ src_go/static/init.js
@@ -1,7 +1,9 @@
-// Initialize the Go WASM code
-const go = new Go();
-const result = await WebAssembly.instantiateStreaming(
-    fetch("main.wasm"),
-    go.importObject
-);
-go.run(result.instance);
+(async () => {
+    // Initialize the Go WASM code
+    const go = new Go();
+    const result = await WebAssembly.instantiateStreaming(
+        fetch("main.wasm"),
+        go.importObject
+    );
+    go.run(result.instance);
+})();

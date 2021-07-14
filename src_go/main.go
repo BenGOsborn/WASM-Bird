24bd71ba4b725671fa41c2a08ebee4819cff2e24
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-14 22:53:26 +1000

    Update

--- src_go/main.go
+++ src_go/main.go
@@ -5,7 +5,30 @@ import (
 	"syscall/js"
 )
 
-func add(this js.Value, args []js.Value) interface{} {
+func addEventListener(elementID string, eventName string, callback func(this js.Value, args []js.Value) interface{}) {
+	js.Global().Get("document").Call("getElementById", elementID).Call("addEventListener", eventName, js.FuncOf(callback))
+}
+
+type Canvas struct {
+	// Declare all of the requirements for the canvas here
+	id  string
+	cvs js.Value
+	ctx js.Value
+}
+
+func NewCanvas(id string) *Canvas {
+	// Initialize the new canvas
+	canvas := new(Canvas)
+
+	// Set the default values
+	canvas.id = id
+	canvas.cvs = js.Global().Get("document").Call("getElementById", id)
+	canvas.ctx = canvas.cvs.Call("getContext", "2d")
+
+	return canvas
+}
+
+func WASMBird(this js.Value, args []js.Value) interface{} {
 	total := 0
 
 	for _, value := range args {
@@ -13,10 +36,6 @@ func add(this js.Value, args []js.Value) interface{} {
 	}
 
 	return js.ValueOf(total)
-}
-
-func addEventListener(elementID string, eventName string, callback func(this js.Value, args []js.Value) interface{}) {
-	js.Global().Get("document").Call("getElementById", elementID).Call("addEventListener", eventName, js.FuncOf(callback))
 }
 
 func main() {

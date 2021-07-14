c85d651b700e182af02e7bf68639f78261e63c13
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-14 13:59:10 +1000

    Update

--- src_go/server.go
+++ src_go/server.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	listen = flag.String("listen", ":3000", "listen address");
+	dir = flag.String("dir", "./static", "directory to serve");
+)
+
 func main() {
+	flag.Parse();
+	log.Printf("Listening on %q...", *listen)
+	log.Fatal(http.ListenAndServe(*listen, http.FileServer(http.Dir(*dir))))
 }

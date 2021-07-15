1d7ed9c831ccbfd7ccb444cd3bd0525845d0b1d5
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-15 13:15:36 +1000

    Update

--- src_go/wasmBird/main.go
+++ src_go/wasmBird/main.go
@@ -27,41 +27,54 @@ func WASMBird(this js.Value, args []js.Value) interface{} {
 	cvs := js.Global().Get("document").Call("getElementById", "canvas")
 	ctx := cvs.Call("getContext", "2d")
 
-	const CVS_WIDTH = cvs.Get("width").Float()
-	const CVS_HEIGHT = cvs.Get("height").Float()
+	CVS_WIDTH := cvs.Get("width").Float()
+	CVS_HEIGHT := cvs.Get("height").Float()
 
 	// Initialize the values of the game
 	const SPEED = 0.5
-	score := 0
+	// score := 0
 	exit := false
-	const GRAVITY = CVS_WIDTH * (0.1 / 100)
+	// GRAVITY := CVS_WIDTH * (0.1 / 100)
 
 	// Declare the values for the pipe
-	const pipeMinHeight = 0.1 * CVS_WIDTH
-	const pipeMaxHeight = 0.6 * CVS_HEIGHT
-	const pipeMinGap = 0.2 * CVS_HEIGHT
-	const pipeMaxGap = 0.3 * CVS_HEIGHT
-	const pipeWidth = 0.2 * CVS_WIDTH
+	// pipeMinHeight := 0.1 * CVS_WIDTH
+	// pipeMaxHeight := 0.6 * CVS_HEIGHT
+	// pipeMinGap := 0.2 * CVS_HEIGHT
+	// pipeMaxGap := 0.3 * CVS_HEIGHT
+	pipeWidth := 0.2 * CVS_WIDTH
 
 	// Adjust according to a logarithmic scale
-	pipeSpacing := 0.5 * CVS_WIDTH
-	dPipeX := CVS_WIDTH * (SPEED / 100)
+	// pipeSpacing := 0.5 * CVS_WIDTH
+	// dPipeX := CVS_WIDTH * (SPEED / 100)
 
 	// Store the pipes for drawing
 	var pipes []*Pipe
 
 	// Declare the values for the bird
-	const birdSize = 0.075 * CVS_WIDTH
-	const birdX = 0.1 * CVS_WIDTH
-	birdY := 0.5 * CVS_HEIGHT
-	dBirdY := CVS_WIDTH * (1 / 100)
+	// birdSize := 0.075 * CVS_WIDTH
+	// birdX := 0.1 * CVS_WIDTH
+	// birdY := 0.5 * CVS_HEIGHT
+	// dBirdY := CVS_WIDTH * (1 / 100)
 
 	for !exit {
 		ctx.Set("fillStyle", "#0099ff")
 		ctx.Call("fillRect", 0, 0, CVS_WIDTH, CVS_HEIGHT)
 		ctx.Set("fillStyle", "#ffcc00")
 		ctx.Call("fillRect", 0, 0.9*CVS_HEIGHT, CVS_WIDTH, CVS_HEIGHT)
-		break
+
+		// Iterate over the pipes and remove the ones that are out of frame
+		tempPipes := []*Pipe{}
+		for _, pipe := range pipes {
+			if pipe.pipeX+pipeWidth > 0 {
+				tempPipes = append(tempPipes, pipe)
+			}
+		}
+		pipes = tempPipes
+
+		// Attempt to add a new pipe
+		if len(pipes) == 0 || CVS_WIDTH-(pipes[len(pipes)-1].pipeX+pipeWidth > 0) {
+			newPipe := new(Pipe)
+		}
 	}
 
 	return nil

2a4164afbd35ac227f37bfa03eff32327d560b08
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-15 11:00:53 +1000

    Update

--- src_go/wasmBird/main.go
+++ src_go/wasmBird/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	// "syscall/js"
+	"fmt"
+	"syscall/js"
 	lib "wasmBird/lib"
 )
 
-// func WASMBird(this js.Value, args []js.Value) interface{} {
-// 	// Event listener for jump
-// 	lib.AddEventListener("keypress", func(this js.Value, args []js.Value) interface{} {
-// 		code := args[0].Get("code").String()
-// 		if code == "Space" {
-// 			fmt.Println("Space!")
-// 		}
-// 		return nil
-// 	})
+func WASMBird(this js.Value, args []js.Value) interface{} {
+	// Event listener for jump
+	lib.AddEventListener("keypress", func(this js.Value, args []js.Value) interface{} {
+		code := args[0].Get("code").String()
+		if code == "Space" {
+			fmt.Println("Space!")
+		}
+		return nil
+	})
 
-// 	return nil
-// }
+	return nil
+}
 
 func main() {
 	// Channel used to prevent program terminating
-	// c := make(chan struct{}, 0)
-
-	lib.Test()
+	c := make(chan struct{}, 0)
 
 	// Initialize the functions in JS
-	// js.Global().Set("WASMBird", js.FuncOf(WASMBird))
+	js.Global().Set("WASMBird", js.FuncOf(WASMBird))
 
 	// Prevent the program from terminating
-	// <-c
+	<-c
 }

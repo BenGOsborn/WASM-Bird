aaa2135696958ae1298d05c3515d87e10f57f34e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-15 22:26:25 +1000

    Update

--- src_go/server.js
+++ src_go/server.js
@@ -1,8 +1,39 @@
 const express = require('express')
+const session = require('express-session')
 
 // Initialize the app and middleware
 const app = express()
 app.use(express.static(__dirname + "/static"));
+app.use(express.json())
+app.use(session({
+    secret: process.env.SECRET || "secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Set the sessions high score
+app.post("/high_score", async (req, res) => {
+    // Get the high score from the request
+    const { high_score } = req.body;
+
+    // Set the high score
+    if (high_score) {
+        req.session.high_score = high_score;
+
+        res.sendStatus(200);
+    } else {
+        res.status(400).send("'high_score' param missing!");
+    }
+});
+
+// Get the sessions high score
+app.get("/high_score", async (req, res) => {
+    if (typeof req.session.high_score !== "undefined") {
+        res.status(200).json({ high_score: req.session.high_score });
+    } else {
+        res.status(400).end("No high score for this session!");
+    }
+});
 
 // Set the port and listen on it
 const PORT = process.env.PORT || 3000;
